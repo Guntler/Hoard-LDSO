d1931343aeee294dd8d1be7089075a1089f3030a
ArmindoBC <ei11111@fe.up.pt>
2014-10-15 11:27:00 +0100

    no message

--- server/server.js
+++ server/server.js
@@ -10,10 +10,11 @@ filesys = require("fs");
 my_http.createServer(function(request,response){  
     var my_path = url.parse(request.url).pathname;  
     var full_path = path.join(process.cwd(),'/../website/');
-	var full_path = path.join(full_path, my_path);
+	full_path = path.join(full_path, my_path);
+	
 
 	//check if path exists
-    path.exists(full_path,function(exists){ 
+    filesys.exists(full_path,function(exists){ 
         if(!exists){  //handle database requests here 
             response.writeHeader(404, {"Content-Type": "text/plain"});    
             response.write("404 Not Found\n");    
@@ -21,7 +22,8 @@ my_http.createServer(function(request,response){
         }  
         else{  
 		
-			//parse file type
+	
+		//parse file type
 			var extname = path.extname(full_path);
 			var contentType = 'text/html';
 
@@ -40,23 +42,25 @@ my_http.createServer(function(request,response){
 					break;
 			}
 			
+
 			//send response
             filesys.readFile(full_path, "binary", function(err, file) {    
                  if(err) {    
                      response.writeHeader(500, {"Content-Type": "text/plain"});    
                      response.write(err + "\n");    
                      response.end();    
-                 
+						
                  }    
                  else{  
                     response.writeHeader(200, {"Content-Type": contentType});    
                     response.write(file, "binary");    
                     response.end();  
-                }  
+					
+						}  
                        
             });  
         }  
     });  
 }).listen(8080);  
-
+	
 sys.puts("Server Running on 8080"); 
