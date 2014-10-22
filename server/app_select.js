8a05f260b627a78d0637c601cb40dbb69696a8fc
ArmindoBC <ei11111@fe.up.pt>
2014-10-22 12:28:19 +0100

    + Database connection

--- server/app_select.js
+++ server/app_select.js
@@ -28,4 +28,39 @@ var client = new pg.Client(conString);
 								client.end();
 						});
 						
+		var query = client.query("SELECT * FROM useraccount ORDER BY userid");
+
+
+	query.on("row", function (row, result) {
+						result.addRow(row);
+					});
+		
+	query.on("end", function (result) {
+								console.log(JSON.stringify(result.rows, null, "    "));
+								client.end();
+						});
+						
+							var query = client.query("SELECT * FROM useraccount ORDER BY userid");
+
+
+	query.on("row", function (row, result) {
+						result.addRow(row);
+					});
+		
+	query.on("end", function (result) {
+								console.log(JSON.stringify(result.rows, null, "    "));
+								client.end();
+						});
+						
+							var query = client.query("SELECT * FROM useraccount ORDER BY userid");
+
+
+	query.on("row", function (row, result) {
+						result.addRow(row);
+					});
+		
+	query.on("end", function (result) {
+								console.log(JSON.stringify(result.rows, null, "    "));
+								client.end();
+						});
 						
