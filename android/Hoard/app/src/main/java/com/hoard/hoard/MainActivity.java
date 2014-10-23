1f9dc083e2bcf2a5d04b0a2aaf460a626bcd371e
André Silva <andre.ee11304@gmail.com>
2014-10-23 15:55:29 +0100

    Android application update.

--- android/Hoard/app/src/main/java/com/hoard/hoard/MainActivity.java
+++ android/Hoard/app/src/main/java/com/hoard/hoard/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends FragmentActivity {
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
     }
 
     @Override
@@ -72,28 +73,4 @@ public class MainActivity extends FragmentActivity {
             return NUM_PAGES;
         }
     }
-
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
-    }*/
 }
--- android/Hoard/app/src/main/java/com/hoard/hoard/ProductSlidePageFragment.java
+++ android/Hoard/app/src/main/java/com/hoard/hoard/ProductSlidePageFragment.java
@@ -9,13 +9,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ProductSlidePageFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.single_product_pageviewer_item_layout, container, false);
 
-        return inflater.inflate(R.layout.single_product_pageviewer_item_layout, container, false);
+        TextView productTextView = (TextView)rootView.findViewById(R.id.product_id);
+
+        productTextView.setText("Product");
+
+        return rootView;
     }
 }
