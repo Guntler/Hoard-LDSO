1060df5e976ceeb03c0b7be94ab40c742cb859cd
Guntler <pbpdirector@hotmail.com>
2014-10-29 14:05:32 +0000

    Fixed sidebars, added product page

--- website/javascript/user/lproducts.js
+++ website/javascript/user/lproducts.js
@@ -1,12 +1,38 @@
 $(document)
 	.ready(function() {
-
+		
+		/*$('.sidebar.edit-profile').sidebar({
+		overlay:true
+		});*/
+		
 		$('.menu .item')
 			.tab()
 		;
 
-		$('.sidebar.edit-profile').first()
-			.sidebar('attach events', '.item.edit-profile.selection')
-		;
+		$('.item.edit-profile.selection').on('click', function() { 
+			var transition = $(this).data('transition');
+				$('.sidebar.edit-profile')
+					.sidebar('setting', {
+				transition       : transition,
+				mobileTransition : transition
+				})
+			.sidebar('toggle');
+			$('.edit-profile.defaultFocus').focus();
+			$(".sidebar.edit-profile").addClass("beingUsed");
+		})
+		
+		$(document).mouseup(function(e) {
+			if (!$(e.target).parents('.signin.sidebar').length && $('.sidebar.edit-profile').hasClass('beingUsed')){
+				$('.sidebar.edit-profile').removeClass('beingUsed');
+				var transition = $(this).data('transition');
+					$('.sidebar.edit-profile')
+						.sidebar('setting', {
+							transition       : transition,
+							mobileTransition : transition
+						})
+				.sidebar('hide');
+			}
+		});
+		
 	})
 ;
