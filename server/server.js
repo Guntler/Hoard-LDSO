ab67541eddc11052bffa09a096abf8b8696bae44
ArmindoBC <ei11111@fe.up.pt>
2014-11-03 11:28:31 +0000

    Implemented Server With Express

--- server/server.js
+++ server/server.js
@@ -116,6 +116,5 @@ my_http.createServer(function(request,response){
 //sys.puts("Server Running on 8080"); 
 =======
 }).listen(8081);  
-
 sys.puts("Server Running on 8080"); 
 >>>>>>> 7ce90e04b682f554a512879e4c379025c96e118c
