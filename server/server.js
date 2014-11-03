b93751aac395d1983569944c1469f419ec548da9
ArmindoBC <ei11111@fe.up.pt>
2014-11-03 11:32:15 +0000

    Conflict in commit resolved

--- server/server.js
+++ server/server.js
@@ -114,7 +114,7 @@ my_http.createServer(function(request,response){
 }).listen(8080);  */
 
 //sys.puts("Server Running on 8080"); 
-=======
-}).listen(8081);  
-sys.puts("Server Running on 8080"); 
->>>>>>> 7ce90e04b682f554a512879e4c379025c96e118c
+//=======
+//}).listen(8081);  
+//sys.puts("Server Running on 8080"); 
+//>>>>>>> 7ce90e04b682f554a512879e4c379025c96e118c
