90cd8124997b6e4c27db07d70b363eaabb350699
unknown <ei11129@I00CS0317.salas.feupsig.fe.up.pt>
2014-11-20 16:49:54 +0000

    Added category functions to server, fixed some stuff and improved the home page and product profile page

---
+++ server/database/utilities.js
@@ -0,0 +1,15 @@
+exports.formatDate = function(value){
+	var month = "";
+	var day = "";
+	
+	if(value.getMonth() < 9)
+		month += "0";
+		
+	month += (value.getMonth()+1);
+		
+	if(value.getDate() < 10)
+		day += "0";
+	day += value.getDate();
+		
+   return  day + "-" + month + "-" + value.getFullYear();
+}
