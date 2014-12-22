d91891d4bea9a9919983b659a621f102e570cac8
jocamar <joca.mar2011@gmail.com>
2014-12-22 22:49:54 +0000

    Added more tests to the login.

--- www/tests/welcome/welcome.test.js
+++ www/tests/welcome/welcome.test.js
@@ -10,7 +10,7 @@ describe('Welcome Page Tests', function(){
 		browser().navigateTo('/');
 	});
 	
-	it('ensures user can log in', function() {
+	it('ensures admin can log in', function() {
 		expect(browser().location().path()).toBe("/");
 
 		element('#hoard-signin').click();
@@ -20,5 +20,53 @@ describe('Welcome Page Tests', function(){
 
 		// logged in route
 		expect(browser().location().path()).toBe("/home/products/1");
+	});
+	
+	it('ensures manager can log in', function() {
+		expect(browser().location().path()).toBe("/");
+
+		element('#hoard-signin').click();
+		input('email').enter('m1@m.com');
+		input('password').enter('m1');
+		element('#signin-submit').click();
+
+		// logged in route
+		expect(browser().location().path()).toBe("/home/products/1");
+	});
+	
+	it('ensures user cant log in', function() {
+		expect(browser().location().path()).toBe("/");
+
+		element('#hoard-signin').click();
+		input('email').enter('u1@u.com');
+		input('password').enter('u1');
+		element('#signin-submit').click();
+
+		expect(browser().location().path()).toBe("/");
+		expect(element("#signin-form").attr('class')).toContain('error');
+	});
+	
+	it('ensures wrong password cant log in', function() {
+		expect(browser().location().path()).toBe("/");
+
+		element('#hoard-signin').click();
+		input('email').enter('m1@m1.com');
+		input('password').enter('m2');
+		element('#signin-submit').click();
+
+		expect(browser().location().path()).toBe("/");
+		expect(element("#signin-form").attr('class')).toContain('error');
+	});
+	
+	it('ensures wrong email cant log in', function() {
+		expect(browser().location().path()).toBe("/");
+
+		element('#hoard-signin').click();
+		input('email').enter('wrong@wrong.com');
+		input('password').enter('m1');
+		element('#signin-submit').click();
+
+		expect(browser().location().path()).toBe("/");
+		expect(element("#signin-form").attr('class')).toContain('error');
 	});
 });
