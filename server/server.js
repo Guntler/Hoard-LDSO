422f47eec35f0ca5e6554721e7bada40d3237524
jocamar <joca.mar2011@gmail.com>
2015-01-04 23:54:53 +0000

    Removed header field.

--- server/server.js
+++ server/server.js
@@ -45,7 +45,6 @@ app.use("/semantic/images", express.static(full_path + '/semantic/images'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
