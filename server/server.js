2cc85d5352b68eac1726e96374c24a3f3710a3e8
jocamar <joca.mar2011@gmail.com>
2015-01-04 02:17:19 +0000

    no message

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use("/semantic/fonts", express.static(full_path + '/semantic/fonts'));
 app.use("/semantic/images", express.static(full_path + '/semantic/images'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "" + req.getHeader("Access-Control-Request-Headers"));
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
