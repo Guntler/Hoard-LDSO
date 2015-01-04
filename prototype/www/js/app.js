2c41932ce64bff7f08da7020a6020effb58146f5
jocamar <joca.mar2011@gmail.com>
2015-01-04 23:26:03 +0000

    Added a header field so server responses.

--- prototype/www/js/app.js
+++ prototype/www/js/app.js
@@ -5,6 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic', 'ngCookies'])
 
+.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+}]);
+
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
